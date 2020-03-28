c4afec785c47e8e1474bb7c7c8b0aea3a8c8b9ed
Matthew Dawson <matthewdawson@outlook.com>
2020-03-28 23:02:27 +1300

    Compute info hash on raw bencode before parsing
    Previously we parsed the value of the info key, bound the result to a
    struct, re-encoded the info struct and then created the hash. This
    caused discrepencies.
    Now we extract the raw bencode from the info and create a sha 1 from
    that.
    
    This commit introduces some tech debt that will have to be addressed:
    Some bytes are being introduced after url encoding with ruby that we are
    directly replacing now. This probably only works using the test torrent
    file we have.
    The url encoding also encodes '%' as '%25' which we are also replacing.

--- plenty-of-fluids/src/main.rs
+++ plenty-of-fluids/src/main.rs
@@ -3,7 +3,8 @@ extern crate bencode;
 
 use rustc_serialize::{Encodable, Decodable};
 
-use bencode::{encode, Decoder, Bencode};
+use bencode::{encode, Decoder, Bencode, ToBencode};
+use bencode::util::ByteString;
 use url::Url;
 use std::collections::*;
 use std::io::prelude::*;
@@ -15,6 +16,7 @@ use std::fs;
 #[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
 struct TorrentMetadata{
     info: TorrentMetadataInfo,
+    info_hash: Vec<u8>,
     announce: String,
 }
 
@@ -30,7 +32,7 @@ fn main() {
     //const TORRENT_PATH: &str = "src/archlinux-2020.02.01-x86_64.iso.torrent";
     const TORRENT_PATH: &str = "test.torrent";
 
-    let bencoded_metadata = fs::read(TORRENT_PATH).unwrap();
+    let bencoded_metadata: Vec<u8> = fs::read(TORRENT_PATH).unwrap();
 
     let bencode: bencode::Bencode = bencode::from_vec(bencoded_metadata).unwrap();
 
@@ -49,13 +51,23 @@ fn main() {
 }
 
 fn parse_torrent_file(bencode: &bencode::Bencode) -> Result<TorrentMetadata, String> {
+    println!("bencode is : {}", bencode);
     let mut top_level_dict = if let Bencode::Dict(dict) = bencode {
         dict.clone()
     } else {
         panic!("top  level bencode should be a dict");
     };
 
-    // println!("tld: {}", bencode);
+    let info_bytes: Vec<u8> = get_field_as_bencoded_bytes(&top_level_dict, "info")?;
+
+    // compute sha
+    let mut hasher = Sha1::new();
+    hasher.input(info_bytes);
+
+    let info_hash = hasher.result().to_vec();
+    println!("new info hash: {:?}", info_hash);
+
+     println!("tld: {}", bencode);
     let announce = top_level_dict.remove(&bencode::util::ByteString::from_str("announce")).unwrap().to_string();
     let info_hashish = top_level_dict.remove(&bencode::util::ByteString::from_str("info")).unwrap();
 
@@ -93,6 +105,7 @@ fn parse_torrent_file(bencode: &bencode::Bencode) -> Result<TorrentMetadata, Str
 
     let mut metadata = TorrentMetadata {
         announce,
+        info_hash,
         info: TorrentMetadataInfo  {
             length: info_length,
             name: info_name,
@@ -106,11 +119,20 @@ fn parse_torrent_file(bencode: &bencode::Bencode) -> Result<TorrentMetadata, Str
 
     // println!("metadata:  {:#?}", metadata);
 
-    for key in top_level_dict.keys() {
+    // for key in top_level_dict.keys() {
         // println!("REMAINING KEY: {}", key);
-    }
+    // }
 
     Ok(metadata)
+}
+
+fn get_field_as_bencoded_bytes(dict: &BTreeMap<bencode::util::ByteString, bencode::Bencode>, field: &str) -> Result<Vec<u8>, String> {
+    let raw_field = match dict.get(&ByteString::from_str(field)) {
+        Some(a) => a,
+        None => return Err("Could not find field".to_string()),
+    };
+
+    Ok(raw_field.to_bytes().unwrap())
 }
 
 fn parse_single_or_multi_file_metadata(bencode: &bencode::Bencode)
@@ -163,21 +185,35 @@ fn build_tracker_query(torrent: TorrentMetadata) ->  Result<url::Url, reqwest::E
         torrent.announce.clone()
     };
 
-    let mut hasher = Sha1::new();
-    hasher.input(encode(&torrent.info).unwrap());
-
-    let hash = hasher.result();
-    // let hash_str = bytes_to_hash_str(&hash);
-    let hash_str = dirty_ruby_urlencode_hack(&hash);
-    let hash_bad_str = bytes_to_hash_str(&hash);
-    // println!("HASH TO USE: '{}'", hash_bad_str);
-
-    //let info_hash: String = url::form_urlencoded::byte_serialize(&hash);
-    // println!("HASH STR: {:?}", hash_str);
+    // let mut hasher = Sha1::new();
+    // // TODO: convert encoded info to text before hashing
+    // let bencode = encode(&torrent.info).unwrap();
+    //
+    // let mut test = bencode.clone().into_iter().map(|x| x as char).collect::<Vec<_>>();
+    // let new_test: String = test.clone().into_iter().collect();
+    //
+    // // let mut info_dict = if let Bencode::Dict(ref dict) = info_hashish { dict.clone() } else { panic!("darn") };
+    // println!("encoded: {:?}", new_test);
+    // // hasher.input(encode(&torrent.info).unwrap());
+    // hasher.input(new_test);
+    //
+    // let hash = hasher.result();
+    // // let hash_str = bytes_to_hash_str(&hash);
+    // println!("Hash pre encoding: {:?}", hash);
+    // // let hash_str = dirty_ruby_urlencode_hack(&hash);
+    // // let hash_bad_str = bytes_to_hash_str(&hash);
+    //  println!("HASH TO USE: '{}'", info_hash);
+    //
+    // println!("Hash post encoding: {}", hash_str);
+    // //let info_hash: String = url::form_urlencoded::byte_serialize(&hash);
+    // // println!("HASH STR: {:?}", hash_str);
+    // println!("Info url {}", &hash_str[..]);
+    //
+    let info_hash = dirty_ruby_urlencode_hack(&torrent.info_hash);
 
     let query = Url::parse_with_params(&formatted_url,
         &[
-        ("info_hash", &hash_str[..]),
+        ("info_hash", &info_hash[..]),
         ("peer_id", "BLARGH1234"),
         ("port", "6881"),
         ("uploaded", "0"),
@@ -187,7 +223,7 @@ fn build_tracker_query(torrent: TorrentMetadata) ->  Result<url::Url, reqwest::E
         ]).unwrap();
 
 
-    // println!("URL: {:?}", query);
+    println!("URL: {:?}", query);
 
     // let resp = reqwest::blocking::get(&query.to_string())?
     //     .json::<HashMap<String, String>>()?;
@@ -195,8 +231,23 @@ fn build_tracker_query(torrent: TorrentMetadata) ->  Result<url::Url, reqwest::E
     Ok(query)
 }
 
+fn encode_info(metadata_info: TorrentMetadataInfo) -> Vec<u8> {
+    // let mut m = BTreeMap::new();
+    // m.insert(ByteString::from_str("pieces"), metadata_info.pieces.to_vec());
+    // m.insert(ByteString::from_str("piece length"), metadata_info.piece_length.to_bencode());
+    // m.insert(ByteString::from_str("length"), Bencode::ByteString(metadata_info.length.to_vec()));
+    // m.insert(ByteString::from_str("name"), Bencode::ByteString(metadata_info.name.to_vec()));
+    // Bencode::Dict(m)
+
+    encode(&metadata_info).unwrap()
+}
+
 fn execute_tracker_query(query: url::Url) -> Result<String, reqwest::Error> {
-    let response = reqwest::blocking::get(&query.to_string())?
+    // Currently the url encoding logic is encoding '%' as '%25'. Need to look into this
+    let sanitized_query = &str::replace(&str::replace(&query.to_string(), "%25", "%"), "%0A", "");
+    //let sanitized_query = &str::replace(&query.to_string(), "%25", "%");
+    println!("query is : {}", sanitized_query);
+    let response = reqwest::blocking::get(sanitized_query)?
         .text();
 
     Ok(response.unwrap())
