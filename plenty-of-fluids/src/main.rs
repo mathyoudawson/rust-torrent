7a3009a0a45fe4ea7f2d4790f65ded0fc241c820
Matthew Dawson <matthewdawson@outlook.com>
2020-04-01 22:05:43 +1300

    Parse response from tracker and extract peers (Only working in test env)
    This commit parses the reponse for the initial anonunce query.
    
    Successfully working for test torrent.
    
    Actual tracker is returning a different response which prevents binding
    into bencode.
    Line of concern: let bencoded_response = bencode::from_vec(response.as_bytes().to_vec()).unwrap();
    
    Some thoughts around this: it looks like the actual torrent returns a
    binary blob for the peers list. Calling as_bytes on a string with binary
    may be issue?
    
    Actual torrent reponse: d8:intervali900e5:peers300:�A��L2�]벐��\u{15}�\u{6}�(�q��\u{1a}�o:���t�X�L����G`�Pc\u{2}\u{17}2���\u{8}Q��G.�nE�;��9V�P��s�ù�e\u{16}��%��2��/\u{74a}�{\u{4}P�e`��l\u{f}-���6%_���>�a���T�0���Q����b�\u{8}E�����,��%��#\'-8l�\u{1b}\u{1c}>�q���U��r�\u{8}�&e�W\rD�f�#\'[@�}��P\u{1}L\u{18}��.�\u{c}\n�y\u{18}�&�0<��р��RL\u{1d}d�)_����s��ZV�N�\u{4}�\u{1f}��`*8!��V�VJ#\'W;�\u{1b}A�S���;n�5��ɲ��\"��R�\u{1d}\u{1a}�-=\rƱ@��8\u{4}�yWy�\u{14}�y��|�\u{1a}���,��&6:peers60:e
    
    Test torrent response: d8:completei0e10:incompletei1e8:intervali600e5:peersld2:ip10:172.17.0.17:peer id20:plenty-of-fluid000014:porti6881eeee

--- plenty-of-fluids/src/main.rs
+++ plenty-of-fluids/src/main.rs
@@ -8,15 +8,12 @@ mod tracker;
 use std::fs;
 
 fn main() {
-    //const TORRENT_PATH: &str = "src/archlinux-2020.02.01-x86_64.iso.torrent";
-    const TORRENT_PATH: &str = "test.torrent";
+    const TORRENT_PATH: &str = "src/archlinux-2020.02.01-x86_64.iso.torrent";
+    //const TORRENT_PATH: &str = "test.torrent";
 
     let bencoded_metadata: Vec<u8> = fs::read(TORRENT_PATH).unwrap();
 
     let metadata = parser::parse_bencoded_torrent(bencoded_metadata).unwrap();
 
-    match tracker::query_tracker(metadata) {
-        Ok(response) => println!("Successful query: {}", response),
-        Err(e) => panic!("Request error: {}", e),
-    }
+    tracker::query_tracker(metadata);
 }
--- plenty-of-fluids/src/tracker.rs
+++ plenty-of-fluids/src/tracker.rs
@@ -3,12 +3,99 @@ extern crate url;
 use super::parser;
 use url::form_urlencoded;
 use std::borrow::Cow;
+use bencode::{Bencode};
+use bencode::util::ByteString;
+use std::str;
+
+pub struct Peer {
+    ip: String, // url crate may give us another type here
+    id: String,
+    port: i64,
+}
 
 pub fn query_tracker(metadata: parser::TorrentMetadata) -> Result<String, reqwest::Error> {
     let query = build_tracker_query(metadata)?;
 
-    execute_tracker_query(query)
+    let response = match execute_tracker_query(query) {
+        Ok(s) => s,
+        Err(e) => panic!("Tracker query failed: {}", e),
+    };
+
+    println!("here is response: {:?}", response);
+
+    let bencoded_response = bencode::from_vec(response.as_bytes().to_vec()).unwrap(); 
+
+    let response_dict = if let Bencode::Dict(dict) = bencoded_response {
+        dict.clone()
+    } else {
+        panic!("top  level bencode should be a dict");
+    };
+
+    let peer_list = match response_dict.get(&bencode::util::ByteString::from_str("peers")).unwrap() {
+        Bencode::List(l) => l,
+        _ => panic!("Not a list"),
+    };
+
+    let mut decoded_peer_list: Vec<Peer> = Vec::new();
+
+    for peer in peer_list {
+        let peer_dict = if let Bencode::Dict(dict) = peer {
+            dict.clone()
+        } else {
+            panic!("top  level bencode should be a dict");
+        };
+
+        let id_bytes: Vec<u8> = match peer_dict.get(&ByteString::from_str("peer id")).unwrap() {
+            Bencode::ByteString(v) => v.to_owned(),
+            _ => panic!("Peer id is not a bytestring"),
+        };
+
+        let id = match String::from_utf8(id_bytes) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        let ip_bytes: Vec<u8> = match peer_dict.get(&ByteString::from_str("ip")).unwrap() {
+            Bencode::ByteString(v) => v.to_owned(),
+            _ => panic!("IP is not a bytestring"),
+        };
+
+        let ip = match String::from_utf8(ip_bytes) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        let port: i64 = match peer_dict.get(&ByteString::from_str("port")).unwrap() {
+            Bencode::Number(n) => n.to_owned(),
+            _ => panic!("Port is not a number"),
+        };
+
+        println!("id: {:?}", id);
+        println!("ip: {:?}", ip);
+        println!("port: {:?}", port);
+
+        decoded_peer_list.push(
+            Peer {
+                ip,
+                id,
+                port,
+        }
+        );
+    }
+
+
+    // let peer_list = unmarshal_peers(peers);
+
+    // interval will dictate how often we are supposed to connect to the tracker to refresh the list of peers. Will be relevant when we open a port and communicate with peers
+    //let interval = response_dict.get(&bencode::util::ByteString::from_str("interval")).unwrap();
+    //println!("interval: {:?}", interval);
+
+    Ok(response)
 }
+
+// fn unmarshal_peers(bencoded_peers: &Bencode) {
+//     const peer_size: u8 = 6;
+// }
 
 fn build_tracker_query(metadata: parser::TorrentMetadata) -> Result<String, reqwest::Error> {
     let formatted_url = if metadata.announce.starts_with("s") {
