cb42a906b186fc7f0b421ebe8d07b7f149d89f3b
Matthew Dawson <matthewdawson@outlook.com>
2020-04-06 20:30:34 +1200

    Get peers from bencoded tracker response
    This commit parses the bencoded tracker response and returns a vector or
    Peer structs. Peers have two attributes, ip and port.
    
    The bencoded response includes a binary blob of the peers. The first 4
    bytes are the array which we join into a string (We might be able to
    represent this better, but will address when we are communicating with
    out peers). The last 2 bytes is the port which is a big endian u16.
    
    This commit caused immense grief as we could not parse the bencode. This
    was due to the crate used for our http query handling. Reqwest would
    return a string that mis-interpretted binary - it looks like it should
    only really be used for plain text.
    A binding of curl crate gave us the response as bytes which we could
    pass into bencode::from_vec(CURL_RESPONSE).
    
    We don't extract out the interval just yet as we don't need it. Interval
    dictates how often we should query the tracker for an updated list of
    peers. I can imagine we will have some thread of background job running
    which will query every interval.

--- plenty-of-fluids/src/main.rs
+++ plenty-of-fluids/src/main.rs
@@ -9,11 +9,21 @@ use std::fs;
 
 fn main() {
     const TORRENT_PATH: &str = "src/archlinux-2020.02.01-x86_64.iso.torrent";
+
+    // TEST TORRENTS
     //const TORRENT_PATH: &str = "test.torrent";
+    //const TORRENT_PATH: &str = "ubuntu-18.04.4-desktop-amd64.iso.torrent";
 
     let bencoded_metadata: Vec<u8> = fs::read(TORRENT_PATH).unwrap();
 
     let metadata = parser::parse_bencoded_torrent(bencoded_metadata).unwrap();
 
-    tracker::query_tracker(metadata);
+    match tracker::get_peers(metadata) {
+        Ok(peers) => {
+            for peer in peers {
+                println!{"{:?}", peer};
+            } 
+        },
+        Err(e) => panic!(e),
+    }
 }
--- plenty-of-fluids/src/tracker.rs
+++ plenty-of-fluids/src/tracker.rs
@@ -4,44 +4,20 @@ use super::parser;
 use url::form_urlencoded;
 use std::borrow::Cow;
 use bencode::{Bencode};
-use bencode::util::ByteString;
-use std::str;
+use curl::easy::Easy;
 
+#[derive(Debug)]
 pub struct Peer {
-    ip: String, // url crate may give us another type here
-    id: String,
-    port: i64,
+    pub ip: String, // url crate may give us another type here
+    pub port: u16,
 }
 
-pub fn query_tracker(metadata: parser::TorrentMetadata) -> Result<String, reqwest::Error> {
+pub fn get_peers(metadata: parser::TorrentMetadata) -> Result<Vec<Peer>, String> {
     let query = build_tracker_query(metadata)?;
 
-    let response = match execute_tracker_query(query) {
-        Ok(s) => s,
-        Err(e) => panic!("Tracker query failed: {}", e),
-    };
+    let response_bytes = execute_tracker_query(query).unwrap();
 
-    println!("here is response: {:?}", response);
-
-    let bytes = response.as_bytes();
-
-    let bencoded_response = match bencode::from_buffer(bytes){
-        Ok(r) => {
-            match r {
-                bencode::Bencode::ByteString(b) => b,
-                _ => panic!("Not a ByteString bro"),
-            }
-        },
-        Err(e) => panic!(e),
-    };
-
-
-    println!("Bencode bytestring: {:?}", bencoded_response);
-
-    let bencoded_response = match bencode::from_vec(response.as_bytes().to_vec()){
-        Ok(a) => a,
-        Err(e) => panic!(e),
-    }; 
+    let bencoded_response = bencode::from_vec(response_bytes).unwrap();
 
     let response_dict = if let Bencode::Dict(dict) = bencoded_response {
         dict.clone()
@@ -50,72 +26,50 @@ pub fn query_tracker(metadata: parser::TorrentMetadata) -> Result<String, reqwes
     };
 
     let peer_list = match response_dict.get(&bencode::util::ByteString::from_str("peers")).unwrap() {
-        Bencode::List(l) => l,
-        _ => panic!("Not a list"),
+        Bencode::ByteString(s) => s,
+        _ => panic!("Not a ByteString"),
     };
 
-    let mut decoded_peer_list: Vec<Peer> = Vec::new();
-
-    for peer in peer_list {
-        let peer_dict = if let Bencode::Dict(dict) = peer {
-            dict.clone()
-        } else {
-            panic!("top  level bencode should be a dict");
-        };
-
-        let id_bytes: Vec<u8> = match peer_dict.get(&ByteString::from_str("peer id")).unwrap() {
-            Bencode::ByteString(v) => v.to_owned(),
-            _ => panic!("Peer id is not a bytestring"),
-        };
-
-        let id = match String::from_utf8(id_bytes) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-
-        let ip_bytes: Vec<u8> = match peer_dict.get(&ByteString::from_str("ip")).unwrap() {
-            Bencode::ByteString(v) => v.to_owned(),
-            _ => panic!("IP is not a bytestring"),
-        };
-
-        let ip = match String::from_utf8(ip_bytes) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-
-        let port: i64 = match peer_dict.get(&ByteString::from_str("port")).unwrap() {
-            Bencode::Number(n) => n.to_owned(),
-            _ => panic!("Port is not a number"),
-        };
-
-        println!("id: {:?}", id);
-        println!("ip: {:?}", ip);
-        println!("port: {:?}", port);
-
-        decoded_peer_list.push(
-            Peer {
-                ip,
-                id,
-                port,
-        }
-        );
-    }
-
-
-    // let peer_list = unmarshal_peers(peers);
-
-    // interval will dictate how often we are supposed to connect to the tracker to refresh the list of peers. Will be relevant when we open a port and communicate with peers
-    //let interval = response_dict.get(&bencode::util::ByteString::from_str("interval")).unwrap();
-    //println!("interval: {:?}", interval);
-
-    Ok(response)
+    unmarshal_peers(peer_list)
 }
 
-// fn unmarshal_peers(bencoded_peers: &Bencode) {
-//     const peer_size: u8 = 6;
-// }
+fn unmarshal_peers(peers: &Vec<u8>) -> Result<Vec<Peer>, String> {
+    const PEER_SIZE: u16 = 6;
+    let mut unmarshalled_peers: Vec<Peer> = Vec::new();
 
-fn build_tracker_query(metadata: parser::TorrentMetadata) -> Result<String, reqwest::Error> {
+    if peers.len() as u16 % PEER_SIZE != 0 {
+        return Err("Received malformed peers".to_string());
+    }
+
+    let peer_chunks: Vec<&[u8]> = peers.chunks(PEER_SIZE as usize).collect();
+
+    for chunk in peer_chunks {
+        unmarshalled_peers.push(Peer{
+            ip: join_nums(chunk[0..4].to_vec(), "."),
+            port: u16::from_be_bytes(try_into_array(&chunk[4..])),
+        });
+    }
+
+    Ok(unmarshalled_peers)
+}
+
+// This function is a sin. try_into will do this for me
+fn try_into_array(slice: &[u8]) -> [u8; 2] {
+    let mut array = [0u8; 2];
+    for (&x, p) in slice.iter().zip(array.iter_mut()) {
+        *p = x;
+    }
+
+    array
+}
+
+fn join_nums(nums: Vec<u8>, sep: &str) -> String {
+   let nums_as_strings: Vec<String> = nums.iter().map(|n| n.to_string()).collect(); 
+
+   nums_as_strings.join(sep)
+}
+
+fn build_tracker_query(metadata: parser::TorrentMetadata) -> Result<String, String> {
     let formatted_url = if metadata.announce.starts_with("s") {
         let mut url: String = metadata.announce.chars().skip(2).collect();
         url.truncate(url.len() - 1);
@@ -147,20 +101,50 @@ fn build_tracker_query(metadata: parser::TorrentMetadata) -> Result<String, reqw
     Ok(query)
 }
 
-fn execute_tracker_query(query: String) -> Result<String, reqwest::Error> {
-    let response = reqwest::blocking::get(&query)?
-        .text_with_charset("utf-8");
+fn execute_tracker_query(query: String) -> Result<Vec<u8>, String> {
+    let mut data = Vec::new();
+    let mut handle = Easy::new();
+    handle.url(&query).unwrap();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|new_data| {
+            data.extend_from_slice(new_data);
+            Ok(new_data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
 
-    Ok(response.unwrap())
+    Ok(data)
 }
 
 #[cfg(test)]
 mod test {
-    //use super::*;
+    use super::*;
 
-    //#[test]
-    // todo
-    // fn get_correct()  {
-    //     assert_eq!()
-    // }
+    #[test]
+    fn correctly_unmarshal_peers()  {
+        let bytes = vec![91, 8, 150, 67, 200, 213, 79, 68, 128, 152, 128, 137];
+        assert_eq!(unmarshal_peers(&bytes).is_ok(), true);
+     }
+
+    // this naming is a hint to implement a from_bytes or something for peer
+    #[test]
+    fn unmarshals_bytes_into_peers() {
+        let bytes = vec![91, 8, 150, 67, 200, 213, 79, 68, 128, 152, 128, 137];
+        let peers = vec![Peer{ip: "91.8.150.67".to_string(), port: 51413},
+                         Peer{ip: "79.68.128.152".to_string(), port: 32905}];
+
+        let unmarshalled_peers = unmarshal_peers(&bytes).unwrap();
+        assert_eq!(unmarshalled_peers[0].ip, peers[0].ip);
+        assert_eq!(unmarshalled_peers[0].port, peers[0].port);
+        assert_eq!(unmarshalled_peers[1].ip, peers[1].ip);
+        assert_eq!(unmarshalled_peers[1].port, peers[1].port);
+    }
+
+    #[test]
+    fn unmarshalling_returns_error_when_bytes_are_malformed(){
+        let bytes = vec![91, 8, 150, 67, 200, 213, 79, 68, 128, 152, 128, 137, 123];
+    
+        assert_eq!(unmarshal_peers(&bytes).unwrap_err(), "Received malformed peers");
+    }
 }
