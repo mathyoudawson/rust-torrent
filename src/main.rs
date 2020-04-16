1a943e9dec8c2e5c13c1e6aea2cecfad06d9ed46
Dylan McKay <me@dylanmckay.io>
2020-04-16 12:07:30 +1200

    Store the test torrent file data as constants rather than load from FS at runtime
    Loading the hardcoded file from the filesystem has two disadvantages:
    
       - `fs::read`, and all the other file APIs, load relative file paths
         from the current working directory of the program - the folder it
         was set up to execute from. This will always be the same as the
         source code folder when running 'cargo run', but will never be when
         running the executable directly (outside of development).
    
       - loading required files at runtime will mean that you have to
         distribute your torrent EXE with its source code. If we load these
        files at compile time instead, there is no need to hit up the
    filesystem at runtime.
    
    This is required if you want a standalone executable to run.

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,23 @@ mod tracker;
 mod peer_connection;
 mod message;
 
-use std::fs;
+/// Bencoded torrent file data fixtures.
+///
+/// These are embedded as constants rather than loaded from the file system so
+/// that the final executable can be distributed on its own, without the source code.
+mod torrent_files {
+    #![allow(dead_code)] // these are currently hardcoded.
+
+    pub const ARCH_LINUX: &'static [u8] = include_bytes!("../res/torrent-file/archlinux-2020.02.01-x86_64.iso.torrent");
+    pub const TEST: &'static [u8] = include_bytes!("../res/torrent-file/test.torrent");
+}
 
 fn main() {
-    const TORRENT_PATH: &str = "src/archlinux-2020.02.01-x86_64.iso.torrent";
-
     // TEST TORRENTS
-    // const TORRENT_PATH: &str = "test.torrent";
-    // const TORRENT_PATH: &str = "ubuntu-18.04.4-desktop-amd64.iso.torrent";
+    // let torrent_file = self::torrent_files::TEST;
+    let torrent_file = self::torrent_files::ARCH_LINUX;
 
-    let bencoded_metadata: Vec<u8> = fs::read(TORRENT_PATH).unwrap();
+    let bencoded_metadata: Vec<u8> = torrent_file.to_owned();
 
     let metadata = parser::parse_bencoded_torrent(bencoded_metadata).unwrap();
 
